0c754dc884f67e29df8a4a53b41b54bc93eae408
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2021-10-30 14:51:58 +0530

    Create LinkingList.py

---
+++ LinkingList.py
@@ -0,0 +1,7 @@
+# Linking two lists
+
+names1 = ['Dev', 'Akash', 'Sujan', 'Yash']
+sub = ['Maths', 'Eating', 'python', 'pubg']
+
+for index, name in enumerate(names1):
+	print(f'{name} likes {sub[index]}')

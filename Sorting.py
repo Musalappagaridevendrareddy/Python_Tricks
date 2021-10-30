9ed52a2f7745ca5fd9114f6110f6c8ce5a9330ea
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2021-10-30 14:59:22 +0530

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -1,9 +1,13 @@
 # Sort list in a single line
 
+# Method 1
+
 l = [4, 2, 1, 3]
 
 list1 = [l.pop(l.index(min(l))) for i in range(len(l))]
 print(list1)
+
+# Method 2
 
 l2 = [1, 8, 4, 5]
 print(sorted(l2))

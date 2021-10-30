947d0f5cfe2bde8dce461e19db98357540e15506
DevendarDev <66067336+Musalappagaridevendrareddy@users.noreply.github.com>
2021-10-30 14:50:21 +0530

    Create GroupingDataInMulLists.py

---
+++ GroupingDataInMulLists.py
@@ -0,0 +1,8 @@
+# Grouping data in multiple lists
+
+l1 = ["Python", "Support", "Don't"]
+l2 = ["Is", "Our", "Forget"]
+l3 = ["Powerful", "Channel", "TO Subscribe"]
+
+for data in zip(l1, l2, l3):
+	print(data)
